5ea4d7a07879b4973d28a8fbcdea54e66b1d8456
StarsCluster <scox@renci.org>
2017-09-01 11:04:00 -0400

    mesos 1.3 alpha

--- cluster/conf/spark/spark-env.sh
+++ cluster/conf/spark/spark-env.sh
@@ -7,7 +7,7 @@ MESOS_NATIVE_JAVA_LIBRARY=/usr/local/lib/libmesos.so
 export MESOS_MASTER=mesos://zk://stars-dc0.edc.renci.org:2181,stars-dc1.edc.renci.org:2181,stars-dc2.edc.renci.org:2181/mesos
 
 export MASTER=$MESOS_MASTER
-export SPARK_EXECUTOR_URI=/projects/stars/stack/v2/spark/spark-2.1.0-bin-hadoop2.6.tgz
+export SPARK_EXECUTOR_URI=/opt/spark-2.1.0-bin-hadoop2.6.tgz
 
 export SPARK_DAEMON_JAVA_OPTS+=""
 
